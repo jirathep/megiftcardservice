c11f77c64b06172b181c990cee60cf9ea302531d
sarun.pn <sarun@promptnow.com>
2025-07-15 10:57:25 +0700

    Fix error RestTemplate

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -19,6 +21,11 @@ public class DemoApplication {
       return "Hello " + name + "!";
     }
   }
+
+  @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
